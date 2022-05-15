bfacb7ec750c6982c28d62016460a7b721f9551c
Martyna Witkowska <mar.witkowska@gmail.com>
2022-05-15 20:01:18 +0200

    added views for html templates

--- donations/views.py
+++ donations/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LandingPageView(TemplateView):
+    template_name = "donations/index.html"
+
+
+class AddDonationView(View):
+
+    def get(self, request):
+        return render(request, "donations/form.html")
+
+
+class LoginView(View):
+
+    def get(self, request):
+        return render(request, "donations/login.html")
+
+
+class RegisterView(View):
+
+    def get(self, request):
+        return render(request, "donations/register.html")
